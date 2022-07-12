55586ebdd3270c238d47bbc614868d627a8ece8f
Estevan Aquino <estevansantosaquino@gmail.com>
2022-07-12 18:03:07 -0300

    dayTwo

--- Components/03_props_and_more/HelloProps/Hello.js
+++ Components/03_props_and_more/HelloProps/Hello.js
@@ -1,7 +1,11 @@
 class Hello extends React.Component {
     render () {
+        let info = "!".repeat(this.props.info)
         return (
-            <p>Hi! To {this.props.to} From {this.props.from}</p> 
+            <div>
+                <p>Hi! To {this.props.to} From {this.props.from}{info}</p> 
+                <img src={this.props.img} />
+            </div>
         )
     }
 }
